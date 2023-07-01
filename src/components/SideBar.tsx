65a3dd22659a17ea2ce7104a38b79bcad9995b6a
Omar <omarinio95@yahoo.com>
2023-07-01 05:34:44 +0200

    working with sidebar + some styling

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
 export default function SideBar() {
-  return <h1>Nav</h1>;
+  const session = useSession();
+  const user = session.data?.user;
+  return (
+    <nav className="navbar">
+      <ul className="navList">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        {user != null && (
+          <li>
+            <Link href={`/profiles/${user.id}`}>Profile</Link>
+          </li>
+        )}
+        {user == null ? (
+          <li>
+            <button onClick={() => void signIn()}>Log In</button>
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => void signOut()}>Log Out</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
